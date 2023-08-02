471bbc3a36c98f4cc009559c3b777ec52144b1e2
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-08-02 21:32:25 +0100

    added react app

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+      <header className="App-header">
+    
+      </header>
+      <footer>
+        Coded by Pri Scarabelli 
+      </footer>
+    </div>
+    </div>
+  );
+}
+
+export default App;
