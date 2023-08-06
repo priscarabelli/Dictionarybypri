af3e6e8f8cdc606044ee4f466711bd37a2ca412a
priscarabelli <125477480+priscarabelli@users.noreply.github.com>
2023-08-06 15:17:26 +0100

    integrated photo API responsive to word search

--- my-app/src/Dictionary.js
+++ my-app/src/Dictionary.js
@@ -1,21 +1,37 @@
 import React, {useState} from "react";
 import "./Dictionary.css"; 
 import Results from "./Results"; 
+import Photos from "./Photos";
 import axios from "axios"; 
+
 
 
 export default function Dictionary(props){
     let [Keyword, setKeyword] = useState(props.defaultWord); 
     let [results , setResults] = useState(null); 
-    let [loaded , setLoaded] = useState(false)
+    let [loaded , setLoaded] = useState(false);
+    let [photos, setPhotos]= useState(null); 
     
-function handleResponse(response){
+function handleDictionaryResponse(response){
    setResults(response.data[0]); 
 }
+
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos); 
+}
+
 function search(){
  // Documentation API: https://dictionaryapi.dev/
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
-axios.get(apiUrl).then(handleResponse);    
+axios.get(apiUrl).then(handleDictionaryResponse);   
+
+let pexelsApiKey = "nyntxpPkp3IRf1bNtMcjgt6kTGtkrffjP5zQnhFqRE9FG89msx50Zq7M";
+let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${Keyword}&per_page=6`;
+axios
+  .get(pexelsApiUrl, {
+    headers: { Authorization: `${pexelsApiKey}` },
+  })
+  .then(handlePexelsResponse);
 }
 
 function handleSubmit(event){
@@ -44,6 +60,7 @@ return( <div className="Dictionary">
     <div className="hint">Suggested words: profound, meaningful, warmth, nature...</div>
      </section>
     <Results results={results}/>
+    <Photos photos={photos}/>
     </div> 
    
     ); 
